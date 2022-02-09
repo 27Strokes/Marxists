3eb34bfa9e9e7000f34de136519e51d0d9f83515
27Strokes <27Strokes@protonmail.com>
2022-02-08 21:27:42 -0800

    change publish.sh

--- publish.sh
+++ publish.sh
@@ -1,10 +1,12 @@
 gitbook install && gitbook build
 
 # checkout to the gh-pages branch
+git branch -D gh-pages
+git branch gh-pages
 git checkout gh-pages
 
 # pull the latest updates
-git pull origin gh-pages --rebase
+#git pull origin gh-pages --rebase
 
 # copy the static site files into the current directory.
 cp -R _book/* .
